6cbde14e104bf6c9f6e6244c644efe0c17bce0d1
ChrstnDmbchr <christiandambacher@gmail.com>
2018-08-16 13:38:26 +0100

    structure modified

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './styling/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Christian Dambacher</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
