45b57907cdba6e4dbadc79ac0f6b7762cb6839d6
ChrstnDmbchr <christiandambacher@gmail.com>
2018-08-16 14:03:20 +0100

    Nav component created

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import '../styling/App.css'
 
+import Nav from './Nav'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Christian Dambacher</h1>
-        </header>
+        <Nav />
       </div>
     );
   }
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styling/Nav.css'
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <h1>Christian Dambacher</h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
