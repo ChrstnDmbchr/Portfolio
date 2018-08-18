2247bba4e0b4a986bf0783a3e56e86fc3ee2d5ce
ChrstnDmbchr <christiandambacher@gmail.com>
2018-08-18 13:15:17 +0100

    nav component partially completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import '../styling/Nav.css'
 
 class Nav extends Component {
+  state = {
+    activeNav: 'about'
+  }
+
+  setActive = menuItem => {
+    this.setState({
+      activeNav: menuItem
+    });
+  };
+
   render() {
+    const { activeNav } = this.state;
     return (
       <div className="nav">
-        <h1>Christian Dambacher</h1>
+        <h1 
+          onClick={() => this.setActive('about')} 
+          className={activeNav === 'about' ? 'nav-item-active' : 'nav-item'}
+        >About</h1>
+
+        <h1 
+          onClick={() => this.setActive('portfolio')} 
+          className={activeNav === 'portfolio' ? 'nav-item-active' : 'nav-item'}
+        >Portfolio</h1>
+
+        <h1 
+          onClick={() => this.setActive('contact')} 
+          className={activeNav === 'contact' ? 'nav-item-active' : 'nav-item'}
+        >Contact</h1>
+        
       </div>
     );
   }
