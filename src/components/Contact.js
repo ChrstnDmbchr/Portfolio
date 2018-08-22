3941ec883fd46b17fb5874bbc74791e421bad2f2
ChrstnDmbchr <christiandambacher@gmail.com>
2018-08-22 14:25:50 +0100

    contact page content added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import '../styling/Contact.css'
 
 class Contact extends Component {
   render() {
     return (
       <div className="contact animated fadeIn">
-        <h1>Contact</h1>
+        <div className="contact-links">
+          <a href="https://www.linkedin.com/in/christian-dambacher-0b988116a"> <i className="about-link fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+          <a href="https://github.com/ChrstnDmbchr"> <i className="fa fa-github fa-3x" aria-hidden="true"></i></a>
+        </div>
+        <h1 className="title contact-email">Email:<a className="contact-email-link" href="mailto:webmaster@example.com">christiandambacher@gmail.com</a></h1>
       </div>
     );
   }
