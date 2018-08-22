fdf897a7d2d723d32dcf44d36db9853650df3c2e
ChrstnDmbchr <christiandambacher@gmail.com>
2018-08-22 20:10:22 +0100

    modified email link and changed wording on portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
       <div className="contact animated fadeIn">
         <h1 className="title">It would be great to hear from you.</h1>
 
-        <h1 className="title contact-email">Email:<a className="contact-email-link" href="mailto:webmaster@example.com">christiandambacher@gmail.com</a></h1>
+        <h1 className="title contact-email">Email:<a className="contact-email-link" href="mailto:christiandambacher@gmail.com">christiandambacher@gmail.com</a></h1>
         <div className="contact-links">
           <a href="https://www.linkedin.com/in/christian-dambacher-0b988116a"> <i className="about-link fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
           <a href="https://github.com/ChrstnDmbchr"> <i className="fa fa-github fa-3x" aria-hidden="true"></i></a>
